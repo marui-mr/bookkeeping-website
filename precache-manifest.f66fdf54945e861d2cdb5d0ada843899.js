7a9fdc086d2b44984e7e00e255e9097a8d1b39db
marui-mr <ma1271968407@163.com>
2021-08-10 22:46:54 +0800

    deploy

---
+++ precache-manifest.f66fdf54945e861d2cdb5d0ada843899.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c759f592f15dbb4bc8b3",
+    "url": "/bookkeeping-website/css/app.5930bb78.css"
+  },
+  {
+    "revision": "6ec33426420827806798f4099d707bf4",
+    "url": "/bookkeeping-website/index.html"
+  },
+  {
+    "revision": "c759f592f15dbb4bc8b3",
+    "url": "/bookkeeping-website/js/app.5d15e50e.js"
+  },
+  {
+    "revision": "b8e64d9db70bca7a5ce4",
+    "url": "/bookkeeping-website/js/chunk-vendors.c8bc7492.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/bookkeeping-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeping-website/robots.txt"
+  }
+]);
